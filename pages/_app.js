c2afd91de24518130d64eb38b44f00f2fe8c2270
Miguel Silva <migueltjsilva@gmail.com>
2021-12-15 09:42:00 -0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,22 @@
+import React from "react"
+import Footer from "../components/footer"
+import { ChakraProvider, CSSReset, ColorModeScript } from "@chakra-ui/react"
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <AuthProvider>
+      
+      <ChakraProvider>
+        <ColorModeScript initialColorMode="light" />
+      
+        <CSSReset />
+        <Component {...pageProps} />
+  
+        <Footer />
+      </ChakraProvider>
+    </AuthProvider>
+  )
 }
 
 export default MyApp
