3b6a476c2a6b12be2ccf79d15caa40902b7da86b
Miguel Silva <migueltjsilva@gmail.com>
2021-12-15 19:38:40 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -27,11 +27,11 @@ export default function Home() {
         <Center>
           <Heading as='h1' fontFamily="arial" marginTop="30px"> Libear </Heading>
         </Center>
-        <SimpleGrid columns={2} spacing={10} marginTop="30px">
-          <GridItem w="50%" h="auto">
+        <SimpleGrid columns={2} spacing={10} marginTop="30px" minChildWidth="sm">
+          <GridItem w="100%" h="auto">
             <Center> <Image src="images/planeta-terra.webp" alt="foto do planeta Terra" /> </Center>
           </GridItem>
-          <GridItem w="50%" h="auto">
+          <GridItem w="100%" h="auto">
             <Center h="100px"><p>Ensino de qualidade</p></Center>
             <Text letterSpacing="2px" textAlign="center">
               O jeito mais inovador de aprender idiomas! Aprender com o Libear é divertido e viciante.
