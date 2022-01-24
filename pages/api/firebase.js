64e8a6a7baf6a8c615514c33d5e545072944e318
Miguel Silva <migueltjsilva@gmail.com>
2022-01-24 18:22:55 -0300

    teste

--- pages/api/firebase.js
+++ pages/api/firebase.js
@@ -17,8 +17,7 @@ if (!firebase.apps.length) {
     });
 }
 
-export default {
-    criarContaFB: async (email, senha) => {
+export async function criarContaFB (email, senha){
         let sucesso = await firebase.auth().createUserWithEmailAndPassword(email, senha).then(() => {
         //   const user = firebase.auth().currentUser;
     
@@ -31,7 +30,6 @@ export default {
         }).catch((error) => {
         //   handleError(error);
         })
-      },
-};
+      }
 
 
