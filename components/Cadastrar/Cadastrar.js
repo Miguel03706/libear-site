5daab91ba0e50cca76ad8e1f7c72c1c759c7de83
Miguel Silva <migueltjsilva@gmail.com>
2022-01-24 11:35:30 -0300

    add img e container
    add imagem para a tela de cadastro

--- components/Cadastrar/Cadastrar.js
+++ components/Cadastrar/Cadastrar.js
@@ -1,10 +1,12 @@
 import React from "react"
+import { Center, Image, Button, Input } from "@chakra-ui/react";
+import styles from "./Cadastrar.module.scss";
 
 export default function Cadastrar(){
 
     return(
-        <>
-         aaa
-        </>
+        <div className={styles.Container}>
+            <Center> <Image src={`icons/logo_urso.webp`}/> </Center>
+        </div>
     );
 }
