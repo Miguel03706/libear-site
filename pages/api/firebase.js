91ea1fdcbd130d296f7170937e373f42b1336324
Miguel Silva <migueltjsilva@gmail.com>
2022-01-24 19:03:08 -0300

    Update firebase.js

--- pages/api/firebase.js
+++ pages/api/firebase.js
@@ -18,7 +18,7 @@ const firebaseConfig = {
   };
   
   // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
+  const firebase = initializeApp(firebaseConfig);
 
 // export default firebase;
 export default {
