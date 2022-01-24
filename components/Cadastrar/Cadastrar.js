bf5b3260ce4330ded6fcf0d07565b7a7c955ba70
Miguel Silva <migueltjsilva@gmail.com>
2022-01-24 11:41:25 -0300

    Update Cadastrar.js

--- components/Cadastrar/Cadastrar.js
+++ components/Cadastrar/Cadastrar.js
@@ -2,11 +2,23 @@ import React from "react"
 import { Center, Image, Button, Input } from "@chakra-ui/react";
 import styles from "./Cadastrar.module.scss";
 
-export default function Cadastrar(){
+export default function Cadastrar() {
 
-    return(
+    return (
         <div className={styles.Container}>
-            <Center> <Image src={`icons/logo_urso.webp`}/> </Center>
+            <Center> <Image src={`icons/logo_urso.webp`} /> </Center>
+            <Center> <h1>Cadastrar</h1></Center>
+
+            <form className={styles.form}>
+                <Center>
+                    <Input
+                        variant="flushed"
+                        type="email"
+                        placeholder="Digite seu email"
+                    />
+                </Center>
+            </form>
+
         </div>
     );
 }
