a4f35185b5745f37e0e4ecd28ceb88800beec656
Miguel Silva <migueltjsilva@gmail.com>
2022-01-24 18:45:04 -0300

    Update firebase.js

--- pages/api/firebase.js
+++ pages/api/firebase.js
@@ -10,6 +10,10 @@ if (!firebase.apps.length) {
         apiKey: process.env.NEXT_PUBLIC_REACT_APP_FIREBASE_KEY,
         authDomain: process.env.NEXT_PUBLIC_REACT_APP_FIREBASE_AUTHDOMAIN,
         projectId: process.env.NEXT_PUBLIC_REACT_APP_FIREBASE_PROJECTID,
+        storageBucket: process.env.NEXT_PUBLIC_REACT_APP_FIREBASE_STORAGEBUCKET,
+        messagingSenderId: process.env.NEXT_PUBLIC_REACT_APP_FIREBASE_MASSEAGINGSENDERID,
+        appId: process.env.NEXT_PUBLIC_REACT_APP_FIREBASE_APPID,
+        measurementId: process.env.NEXT_PUBLIC_REACT_APP_FIREBASE_MEASUREMENTID
     });
 }
 
